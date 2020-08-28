f8878709de84510c3899bd9bb6b78f6692cf21dc
Jorge López Gil <wiijlg@hotmail.com>
2020-08-28 22:02:53 +0200

    Movie Model

---
+++ lib/src/models/movie_model.dart
@@ -0,0 +1,33 @@
+class Movie {
+  double popularity;
+  int voteCount;
+  bool video;
+  String posterPath;
+  int id;
+  bool adult;
+  String backdropPath;
+  String originalLanguage;
+  String originalTitle;
+  List<int> genreIds;
+  String title;
+  double voteAverage;
+  String overview;
+  String releaseDate;
+
+  Movie({
+    this.popularity,
+    this.voteCount,
+    this.video,
+    this.posterPath,
+    this.id,
+    this.adult,
+    this.backdropPath,
+    this.originalLanguage,
+    this.originalTitle,
+    this.genreIds,
+    this.title,
+    this.voteAverage,
+    this.overview,
+    this.releaseDate,
+  });
+}
