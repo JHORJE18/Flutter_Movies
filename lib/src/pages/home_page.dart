f0138898902757d67716c718d6e04f6be85ec187
Jorge López Gil <wiijlg@hotmail.com>
2020-08-28 21:33:51 +0200

    Swiper

--- lib/src/pages/home_page.dart
+++ lib/src/pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_swiper/flutter_swiper.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({Key key}) : super(key: key);
@@ -6,9 +7,38 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: SafeArea(
-        child: Text('Hola'),
+      appBar: AppBar(
+        title: Text('Películas 🎬'),
+        backgroundColor: Colors.indigoAccent,
+        actions: [
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {},
+          )
+        ],
       ),
+      body: SafeArea(
+        child: Column(
+          children: [_swipeTarjetas(context)],
+        ),
+      ),
+    );
+  }
+
+  Widget _swipeTarjetas(BuildContext context) {
+    return Container(
+      height: 350,
+      width: double.infinity,
+      child: new Swiper(
+          itemBuilder: (BuildContext context, int index) {
+            return new Image.network(
+              "http://via.placeholder.com/350x150",
+              fit: BoxFit.fill,
+            );
+          },
+          itemCount: 3,
+          itemWidth: 300.0,
+          layout: SwiperLayout.STACK),
     );
   }
 }
