9e72586b4b772083cda55e555e0737c67e5f1075
Jorge López Gil <wiijlg@hotmail.com>
2020-08-31 20:47:11 +0200

    Search delegate function

--- lib/src/search/search_delegate.dart
+++ lib/src/search/search_delegate.dart
@@ -1,6 +1,18 @@
 import 'package:flutter/material.dart';
 
 class DataSearch extends SearchDelegate {
+  String seleccion = '';
+
+  final peliculas = [
+    'Spiderman',
+    'Regreso al futuro',
+    'Doctor Who',
+    'Cars',
+    'Cars 2',
+    'Cars 3'
+  ];
+  final peliculasRecientes = ['Spiderman', 'Capitan america'];
+
   @override
   List<Widget> buildActions(BuildContext context) {
     // Acciones de nuestro AppBar
@@ -31,12 +43,54 @@ class DataSearch extends SearchDelegate {
   @override
   Widget buildResults(BuildContext context) {
     // Crea los resultado a mostrar
-    return Container();
+    return Center(
+      child: Container(
+        height: 100,
+        width: 100,
+        color: Colors.amberAccent,
+        child: Center(child: Text(seleccion)),
+      ),
+    );
   }
 
   @override
   Widget buildSuggestions(BuildContext context) {
     // Surgerencias que aparecen cuando la persona escribe
-    return Container();
+
+    final listaSurgerencia = (query.isEmpty)
+        ? peliculasRecientes
+        : peliculas
+            .where((element) =>
+                element.toLowerCase().startsWith(query.toLowerCase()))
+            .toList();
+
+    if (listaSurgerencia.length == 0) {
+      return Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Icon(Icons.mood_bad),
+            SizedBox(
+              height: 10,
+            ),
+            Text('Sin resultados'),
+          ],
+        ),
+      );
+    }
+
+    return ListView.builder(
+      itemCount: listaSurgerencia.length,
+      itemBuilder: (context, index) {
+        return ListTile(
+          leading: Icon(Icons.movie_filter),
+          title: Text(listaSurgerencia[index]),
+          onTap: () {
+            seleccion = listaSurgerencia[index];
+            showResults(context);
+          },
+        );
+      },
+    );
   }
 }
